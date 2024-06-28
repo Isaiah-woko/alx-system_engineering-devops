c2c949ecfacfe6de02dc7f3d24993c886b0654b3
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-28 19:10:09 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
-# This Puppet manifest ensures that the Flask package version 2.1.0 is installed using pip3
-ile { '/tmp/school':
+# This Puppet manifest creates a file in /tmp with specified properties
+file { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
