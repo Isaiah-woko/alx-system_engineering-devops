10849be854605a809e5fb23becef70750a074229
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-28 18:59:17 +0100

    okay

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,5 @@
-file { '/tmp/school':
+# This Puppet manifest ensures that the Flask package version 2.1.0 is installed using pip3
+ile { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# This Puppet manifest 
 exec { 'kill_killmenow_process':
   command => 'pkill -f killmenow',
   path    => ['/bin', '/usr/bin'],
