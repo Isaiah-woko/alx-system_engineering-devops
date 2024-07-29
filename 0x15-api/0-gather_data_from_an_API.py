e3cfa672cb6935e0f3f1b4335cc7c270781ba418
Isaiah woko <wokoisaiah68@gmail.com>
2024-07-29 21:44:31 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,29 +5,35 @@
 
 import requests
 import sys
+import re
+
+REST_API_URL = "https://jsonplaceholder.typicode.com"
 
 
 def todo_list_progress(employee_ID):
     """getting the todolist"""
 
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    todo_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    user_url = f'{REST_API_URL}/users/{employee_id}'
+    todo_url = f'{REST_API_URL}/todos?userId={employee_id}'
 
+    # Get reponses
     user_response = requests.get(user_url)
+    todo_response = requests.get(todo_url)
+
+    if user_response.status_code != 200 or todo_response.status_code != 200:
+        return None
+
     user_data = user_response.json()
     employee_name = user_data['name']
 
-    # Todo list
-    todo_reponse = requests.get(todo_url)
-
-    todos = todos_response.json()
+    todos = todo_response.json()
     total_tasks = len(todos)
-    done_tasks = [todo for todo in todos if todo['completed']]
+    done_tasks = [todo_task for todo_task in todos if todo_task['completed']]
     number_of_done_tasks = len(done_tasks)
 
     # display result
-    print(
-        f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}): ")
+    print('Employee {} is done with tasks({}/{}):'.format(
+        employee_name, number_of_done_tasks, total_tasks))
 
     # Display the titles of completed tasks
     for task in done_tasks:
@@ -36,13 +42,10 @@ def todo_list_progress(employee_ID):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
         sys.exit(1)
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Error: Employee ID must be an integer.")
+    if not re.fullmatch(r'\d+', sys.argv[1]):
         sys.exit(1)
 
+    employee_id = int(sys.argv[1])
     todo_list_progress(employee_id)
